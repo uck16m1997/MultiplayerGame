63eccfd6c7f4812c8cdec7c164261bd55a66c507
uck16m1997 <uckaya6@gmail.com>
2024-11-18 21:47:16 +0300

    Multiplayer starter code

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField] private float _moveSpeed = 4f;
+    private Camera _mainCamera;
+    private Vector3 _mouseInput = Vector3.zero;
+
+
+    public override void OnNetworkSpawn() {
+        base.OnNetworkSpawn();
+        Initialize();
+    }
+
+    private void Initialize() {
+        _mainCamera = Camera.main;
+    }
+    
+    private void Update() {
+        if(!IsOwner || !Application.isFocused) return;
+        
+        _mouseInput = Input.mousePosition;
+        _mouseInput.z = _mainCamera.nearClipPlane;
+        
+        Vector3 mouseWorldCoordinates = _mainCamera.ScreenToWorldPoint(_mouseInput);
+        
+        transform.position = Vector3.MoveTowards(transform.position, mouseWorldCoordinates,Time.deltaTime * _moveSpeed);
+
+        if (mouseWorldCoordinates != transform.position) {
+            Vector3 targetDirection = mouseWorldCoordinates - transform.position;
+            targetDirection.z = 0;
+            transform.up = targetDirection;
+        }
+    }
+
+    private void ClientAuthoratative() {
+
+    }
+}
---
+++ Assets/Scripts/StartNetwork.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class StartNetwork : MonoBehaviour
+{
+    public void StartServer() {
+        NetworkManager.Singleton.StartServer();
+    }   
+    public void StartClient() {
+        NetworkManager.Singleton.StartClient();
+    }   
+    public void StartHost() {
+        NetworkManager.Singleton.StartHost();
+    }
+}
